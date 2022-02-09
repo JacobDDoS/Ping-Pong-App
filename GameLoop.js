94351d8678f9ed6dbfb55baaa2ae57ecc9af8e19
JacobDDoS <jacdimmitt@gmail.com>
2022-02-08 20:33:17 -0600

    Created smoother transitions and allowed ball to go past paddle

--- GameLoop.js
+++ GameLoop.js
@@ -1,9 +1,12 @@
 import Constants from "./Constants";
 let isUserPressingDownButton = false;
 let isUserPressingUpButton = false;
-let ballSpeedX = -1;
-let ballSpeedY = 0;
-let ballRefreshSpeedUp = 0;
+let ballSpeedX = -0.2;
+let ballSpeedY = 0.0;
+let ballSpeedUp = 0.01;
+let paddleSpeed = 0.2;
+let paddleSpeedUp = 0.01
+let hasBallGonePastPaddle = false;
 const GameLoop = (entities, { touches, dispatch, events}) => {
     let playerPaddle = entities.playerPaddle
     let ball = entities.ball
@@ -24,60 +27,71 @@ const GameLoop = (entities, { touches, dispatch, events}) => {
     }
 
     //Move player's paddle 
-    playerPaddle.nextMove -= 2;
-    if (playerPaddle.nextMove <= 0) {
-        playerPaddle.nextMove = playerPaddle.updateFrequency;
-        if (!(playerPaddle.position[0]+4 >= Constants.Y_GRID_SIZE) && isUserPressingUpButton){
-            // playerPaddle.xspeed = 1;
-            playerPaddle.position[0]+=1;
-        } else if (!(playerPaddle.position[0] <= 0) && isUserPressingDownButton) {
-            playerPaddle.position[0]-=1;
-        }
-    } 
+    // playerPaddle.nextMove -= 2;
+    // if (playerPaddle.nextMove <= 0) {
+    // playerPaddle.nextMove = playerPaddle.updateFrequency;
+    if (!(Math.floor(playerPaddle.position[0])+4+paddleSpeed > Constants.Y_GRID_SIZE) && isUserPressingUpButton){
+        // playerPaddle.xspeed = 1;
+        playerPaddle.position[0]+=paddleSpeed;
+    } else if (!(playerPaddle.position[0]-paddleSpeed <= 0) && isUserPressingDownButton) {
+        playerPaddle.position[0]-=paddleSpeed;
+    } else if (isUserPressingUpButton) {
+        playerPaddle.position[0] = Constants.Y_GRID_SIZE - 4;
+    } else if (isUserPressingDownButton) {
+        playerPaddle.position[0] = 0;
+    }
+    // } 
 
     //Move ball
-    ball.nextMove -= 1.25;
-    if (ball.nextMove <= 0) {
-        ball.nextMove = Constants.BALL_REFRESH_RATE - ballRefreshSpeedUp;
-        ball.position[0] += ballSpeedY;
-        ball.position[1] += ballSpeedX;
-        ball.position[1] = Math.max(1, ball.position[1])
-        ball.position[1] = Math.min(Constants.X_GRID_SIZE-1, ball.position[1])
-        ball.position[0] = Math.max(0, ball.position[0])
-        ball.position[0] = Math.min(Constants.Y_GRID_SIZE-1, ball.position[0])
+    // ball.nextMove -= 1.25;
+    // if (ball.nextMove <= 0) {
+        // ball.nextMove = Constants.BALL_REFRESH_RATE - ballRefreshSpeedUp;
+    ball.position[0] += ballSpeedY;
+    ball.position[1] += ballSpeedX;
+    if (!hasBallGonePastPaddle) {
+        ball.position[1] = Math.max(0.98, ball.position[1])
+    }
+    ball.position[1] = Math.min(Constants.X_GRID_SIZE-1, ball.position[1])
+    ball.position[0] = Math.max(0, ball.position[0])
+    ball.position[0] = Math.min(Constants.Y_GRID_SIZE-1, ball.position[0])
 
-        //Check to see if ball is hitting paddle
-        if (ball.position[1] <= 1.3) {
-            if (playerPaddle.position[0] <= ball.position[0] && ball.position[0] <= playerPaddle.position[0]+4) {
-                ballSpeedX *= -1;
-                ballSpeedY += Math.random() / 2;
-                ballRefreshSpeedUp += Constants.BALL_REFRESH_RATE_SPEED_UP_RATE
-            } else {
-                // Player lost
-                ballSpeedX = -1;
-                ballSpeedY = 0;
-                ball.position[0] = 10;
-                ball.position[1] = 13;
-                ballRefreshSpeedUp = 0;
-            }
-        }
-
-        //Check to see if ball is hitting top wall
-        if (ball.position[0]>=Constants.Y_GRID_SIZE-1.3) {
-            ballSpeedY *= -1;
-            ballRefreshSpeedUp += Constants.BALL_REFRESH_RATE_SPEED_UP_RATE
-        }
-        //Check to see if ball is hitting right wall
-        if (ball.position[1]>=Constants.X_GRID_SIZE-1.3) {
+    //Check to see if ball is hitting paddle
+    if (ball.position[1] == 0.98) {
+        if ((playerPaddle.position[0] <= ball.position[0]+1 && ball.position[0] <= playerPaddle.position[0]+4)) {
             ballSpeedX *= -1;
-            ballRefreshSpeedUp += Constants.BALL_REFRESH_RATE_SPEED_UP_RATE
-        }
-        //Check to see if ball is hitting bottom wall
-        if (ball.position[0]<=0.3) {
-            ballSpeedY *= -1;
-            ballRefreshSpeedUp += Constants.BALL_REFRESH_RATE_SPEED_UP_RATE
+            ballSpeedY += (Math.random() - 0.5)/5;
+            ballSpeedX += ballSpeedUp;
+            paddleSpeed += paddleSpeedUp;
+        } else {
+            // Ball has gone past paddle
+            hasBallGonePastPaddle = true;
         }
     }
+
+    //Check to see if user has lost
+    if (ball.position[1] <= -0.2) {
+        ballSpeedX = -0.2;
+        ballSpeedY = 0.0;
+        ball.position[0] = 10;
+        ball.position[1] = 13;
+        paddleSpeed = 0.2;
+        hasBallGonePastPaddle = false;
+    }
+
+    //Check to see if ball is hitting top wall
+    if (ball.position[0]>=Constants.Y_GRID_SIZE-1) {
+        ballSpeedY *= -1;
+    }
+    //Check to see if ball is hitting right wall
+    // console.log(ball.position[1])
+    if (ball.position[1]>=Constants.X_GRID_SIZE-1) {
+        ballSpeedX *= -1;
+    }
+    //Check to see if ball is hitting bottom wall
+    if (ball.position[0]<=0) {
+        ballSpeedY *= -1;
+    }
+// }
 
     return entities;
 }
